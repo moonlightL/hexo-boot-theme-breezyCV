ce09471be50c5f900251ab5b6b26ebb68c65f27f
moonlightL <jx8996@163.com>
2021-07-22 16:34:26 +0800

    调整

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 (function($) {
     "use strict";
+
     function portfolio_init() {
         var portfolio_grid = $('.portfolio-grid'),
             portfolio_filter = $('.portfolio-filters');
@@ -191,43 +192,6 @@
                         $(this).parent('.form-group').removeClass('form-group-focus');
                     }
                 });
-            $('body').magnificPopup({
-                delegate: 'a.lightbox',
-                type: 'image',
-                removalDelay: 300,
-                mainClass: 'mfp-fade',
-                image: {
-                    titleSrc: 'title',
-                    gallery: {
-                        enabled: true
-                    },
-                },
-                iframe: {
-                    markup: '<div class="mfp-iframe-scaler">' + '<div class="mfp-close"></div>' + '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' + '<div class="mfp-title mfp-bottom-iframe-title"></div>' + '</div>',
-                    patterns: {
-                        youtube: {
-                            index: 'youtube.com/',
-                            id: null,
-                            src: '%id%?autoplay=1'
-                        },
-                        vimeo: {
-                            index: 'vimeo.com/',
-                            id: '/',
-                            src: '//player.vimeo.com/video/%id%?autoplay=1'
-                        },
-                        gmaps: {
-                            index: '//maps.google.',
-                            src: '%id%&output=embed'
-                        }
-                    },
-                    srcAction: 'iframe_src',
-                },
-                callbacks: {
-                    markupParse: function(template, values, item) {
-                        values.title = item.el.attr('title');
-                    }
-                },
-            });
-
         });
+
 })(jQuery);
